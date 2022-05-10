3e183a2b6ec83c6dd9542327ef1d89c779cbdb6c
icodesrinivas@gmail.com <icodesrinivas@gmail.com>
2022-05-10 20:55:45 +0530

    Adding all design principles inside folder

--- DesignPrinciples/DependencyInversionPrinciple.py
+++ DesignPrinciples/DependencyInversionPrinciple.py
@@ -1,12 +1,19 @@
+# Dependency Inversion Principle
+
+# One should depend upon abstraction and not concretion
+
+# High level modules should not depend upon the low level modules
 
 from enum import Enum
-from abc import abstractmethod
+from abc import ABC, abstractmethod
 
 class Relationship(Enum):
+
     PARENT = 0
     CHILD = 0
 
 class Person:
+
     def __init__(self, name):
         self.name = name
 
@@ -14,9 +21,11 @@ class RelationshipBrowser:
     @abstractmethod
     def find_all_children_of(self, name): pass
 
-class Relationships(RelationshipBrowser):
+class Relationships(RelationshipBrowser): # Low Level Module
+
     def __init__(self):
         self.relations = []
+
 
     def add_parent_and_child(self, parent, child):
         self.relations.append(
@@ -31,18 +40,17 @@ class Relationships(RelationshipBrowser):
             if r[0].name == name and r[1] == Relationship.PARENT:
                 yield r[2].name
 
-class Research:
-
+class Research: # High Level Module
     # def __init__(self, relationships):
     #     relations = relationships.relations
+    #
     #     for r in relations:
     #         if r[0].name == 'John' and r[1] == Relationship.PARENT:
-    #             print(f'John has a child called {r[2].name}.')
+    #             print(f'John has a child with name {r[2].name}.')
 
     def __init__(self, browser):
         for p in browser.find_all_children_of('John'):
-            print(f'John has a child called {p}')
-
+            print(f'John has a child with name {p}')
 
 parent = Person('John')
 child1 = Person('Mike')
@@ -53,3 +61,6 @@ relationships.add_parent_and_child(parent, child1)
 relationships.add_parent_and_child(parent, child2)
 
 Research(relationships)
+
+
+
